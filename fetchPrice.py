291124f62f7dcb5bd15270f322a70b1fb76eafd1
mikepocket <taschner.michael@gmail.com>
2021-02-24 22:28:35 +0100

    change due to deployment error

--- fetchPrice.py
+++ fetchPrice.py
@@ -1,9 +1,9 @@
 from pycoingecko import CoinGeckoAPI
 
-cg = CoinGeckoAPI()
-markets = cg.get_coins_markets(vs_currency='usd')
+def fetch():
+	cg = CoinGeckoAPI()
+	markets = cg.get_coins_markets(vs_currency='usd')
 
-print("# of markets = ", len(markets))
+	return markets
 
-
-
+fetch()
